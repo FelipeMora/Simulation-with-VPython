8459acbb89bc2e96874546c57158b57eebb10fb9
FelipeMora <fmora51@ctb.edu.co>
2017-10-31 22:14:30 -0500

    Lectura de datos con pymongo

--- Sending_Data.py
+++ Sending_Data.py
@@ -1,20 +1,30 @@
 #Debera realizar la conexion con la bd y realizar todoas las respectivas consultas
 from pymongo import MongoClient
-
+import time
 
 class crud_mongo:
     #Host = localhost
     #Puerto = 27017
     #Nombre BD : Posicuerpos #user,pas
-    def __init__(self,host,puerto,bd,dictionary,collection_set,operation):
-        print(">>>>>>>>>>")*20
+    def __init__(self,dictionary,collection_set,operation):
+        self.recorrido = 0#No deberia estar aca
+        self.__group = 0
         self.__dictionary = dictionary
-        #En linea
-        #bd_uri = 'mongodb://{}:{}@ds235775.mlab.com:35775/posicuerpos'.format(user,pas)
-        self.__mongoClient = MongoClient()
-        self.db = self.__mongoClient.bd
-        self.classi_operation(operation)
+        self.__mongoClient = MongoClient('ds235775.mlab.com',35775)
+        self.db = self.__mongoClient.posicuerpos
+        self.db.authenticate('Simulation','1234567')
+        self.classi_operation(operation)#r
+        switcher = {
+            "body_data" : self.body_data,
+            "processed_info" : self.proc_info,
+            "ball" : self.body_data_ball,
+            "wall" : self.body_data_wall,
+            "grupo_general" : self.group_general
+        }
+        site = switcher.get(collection_set)
+        site()
         self.go(collection_set)#Posible problema logico
+        self.closeSession()
 
     def classi_operation(self,arg):
         switcher = {
@@ -24,37 +34,45 @@ class crud_mongo:
         self.go = switcher.get(arg)
     #@classmethod
     def create(self,collection):#Toca clasificar la collection a la que va
-        print("<<<<<<<<<<<<<<<<<<<< CREATE >>>>>>>>>>>>>>>>>")*20
-        switcher = {
-            "body_data" : self.create_body_data,
-            "processed_info" : self.create_proc_info,
-            "ball" : self.create_body_data_ball,
-            "wall" : self.create_body_data_wall
+        self.collection.insert(self.__dictionary)
+
+    def body_data(self):
+        self.collection = self.db.body_data
+
+    def proc_info(self):#No esta completo
+        self.collection = self.db.processed_info
+
+    def body_data_ball(self):
+        self.collection = self.db.ball
+
+    def body_data_wall(self):
+        self.collection = self.db.wall
+
+    def group_general(self):
+        self.collection = self.db.grupo_general
+
+    def read(self,collection):
+        if self.recorrido != 1:#Self recorrido podria estar en el main
+            self.recorrido = 1
+            try:
+                self.Cursor = self.collection.find({},{"fecha":0,"_id":0}).limit(1).sort("grupo", -1)
+                if self.Cursor != "NONE":
+                    for fut in self.Cursor:
+                        print('En for')
+                        jk = fut['grupo']
+                        self.__group = int(jk) + 1
+                        break
+            except:
+                print("Hubo un problema")
+            dat_group = self.group_data_general(self.__group)
+            crud_mongo(dat_group,'grupo_general',"c")
+
+    def group_data_general(self,g):
+        fecha = time.strftime("%c")
+        return {
+            "fecha" : fecha,
+            "grupo": g
         }
-        site = switcher.get(collection)
-        collection_cla = site()
-        collection_cla.insert(self.__dictionary)
-        print("<<<<<<<<<<<<<<< COLECCION INGRESADA >>>>>>>>>>>>>>>>>>")*20
-        self.closeSession()
-
-    def create_body_data(self):
-        collection = self.db.body_data
-        return collection
-
-    def create_proc_info(self):#No esta completo
-        collection = self.db.processed_info
-        return collection
-
-    def create_body_data_ball(self):
-        collection = self.db.ball
-        return collection
-
-    def create_body_data_wall(self):
-        collection = self.db.wall
-        return collection
-
-    def read(self):
-        print("Aun no se leer la BD, lo siento :C")
 
     def closeSession(self):
         self.__mongoClient.close()
